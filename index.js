8edf125948edb21410f96e49439f9b5d208491f2
ssivaji-gb <sathirabathisivajir@gmail.com>
2025-12-04 21:38:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+let userName = document.querySelector(".userName");
+let Email = document.querySelector(".Email");
+let PassWord = document.querySelector(".PassWord");
+let Retype = document.querySelector(".Retype");
+let formGrp = document.querySelectorAll(".formGrp");
+let form = document.querySelector("form");
+
+form.addEventListener("submit", butTon);
+
+function butTon() {
+  let inputs = document.querySelectorAll("input");
+
+  inputs.forEach((element) => {
+    let parent = element.parentElement;
+    console.log(parent);
+    if (element.value === "") {
+      parent.classList.add("required");
+    } else if (element.value !== "") {
+      parent.classList.remove("required");
+      parent.classList.add("success");
+    }
+     let email=document.querySelector(".Email")
+
+     if(email.value.includes("@")&&email.value.includes(".")){
+        email.parentElement.classList.add("success")
+     }else if(!email.value.includes("@")&&!email.value.includes(".")&&email.value!=="") {
+        email.parentElement.classList.remove("success")
+        email.parentElement.classList.add("required")
+        email.parentElement.lastElementChild.innerHTML="Enter the Email Properly using @ and . "
+     }
+     if(Retype.value === PassWord.value){
+      Retype.parentElement.classList.add("success")
+      Retype.parentElement.classList.remove("required")
+     }else{
+      Retype.parentElement.classList.remove("success")
+      Retype.parentElement.classList.add("required")
+       let msg = Retype.parentElement.querySelector("p");
+    msg.innerHTML = "Password do not match";
+     }
+     
+
+  });
+
+  // })
+}
